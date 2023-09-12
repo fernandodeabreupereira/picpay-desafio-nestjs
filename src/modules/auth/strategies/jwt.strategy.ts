74bbedb71b3f2bf8ad414b9a77de49b28b83a960
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-12 11:12:06 -0300

    chore: implements strategy local

---
+++ src/modules/auth/strategies/jwt.strategy.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { IUserJWT } from '../models/user-jwt';
+import { IUserPayload } from '../models/user-payload';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor () {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: process.env.JWT_SECRET,
+    });
+  }
+
+  async validate (payload: IUserPayload): Promise<IUserJWT> {
+    return {
+      id: payload.sub,
+      role: payload.role,
+      full_name: payload.full_name,
+      email: payload.email,
+    };
+  }
+}
