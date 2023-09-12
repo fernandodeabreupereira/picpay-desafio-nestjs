a6fef7cc455cc269a61f650720ae4708ca59e8da
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-12 11:12:42 -0300

    feat: create module user

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,16 @@
-import { AuthModule } from './modules/auth/auth.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { APP_GUARD } from '@nestjs/core';
+import { AuthModule } from './modules/auth/auth.module';
+import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
+    UserModule,
     AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    AuthModule,
   ],
   providers: [
     // {
