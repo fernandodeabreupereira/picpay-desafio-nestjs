a4e951bb9aaa073e7f84d81494a5fc0ca03c8fd9
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-12 11:13:08 -0300

    chore: configure auth module

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,8 +1,16 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from "@nestjs/common";
+import { UserModule } from "../user/user.module";
+import { LoginValidationBodyMiddleware } from "./middlewares/login-validation-body.middleware";
+import { AuthService } from "./services/auth.service";
+import { JwtStrategy } from "./strategies/jwt.strategy";
+import { LocalStrategy } from "./strategies/local.strategy";
 
 @Module({
-    imports: [],
-    controllers: [],
-    providers: [],
+    imports: [UserModule],
+    providers: [AuthService, LocalStrategy, JwtStrategy],
 })
-export class AuthModule { }
+export class AuthModule implements NestModule {
+    configure (consumer: MiddlewareConsumer) {
+        consumer.apply(LoginValidationBodyMiddleware).forRoutes('auth/login');
+    }
+}
