cbd3191cc6c3a2aec1da6d6bd01f845996971193
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-12 11:33:48 -0300

    chore: create module login validation body

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { LoginValidationBodyModule } from './modules/login-validation-body/login-validation-body.module';
 import { UserModule } from './modules/user/user.module';
 
 @Module({
   imports: [
-    UserModule,
-    AuthModule,
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
+    UserModule,
+    AuthModule,
+    LoginValidationBodyModule,
   ],
   providers: [
     // {
