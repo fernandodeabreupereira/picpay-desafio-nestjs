43e2d476925c2c6e7fe6bbeac26d5778e6355e66
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-12 16:24:31 -0300

    feat: create module test dependencies and repository user sql memory

---
+++ src/modules/test/test-dependencies.module.ts
@@ -0,0 +1,17 @@
+import { Global, Module } from '@nestjs/common';
+import { UserRepository } from '../../repositories/abstracts/user.repository';
+import { UserModule } from '../user/user.module';
+import { UserRepositorySQLInMemory } from './sql-in-memory/user-repository-sql-in-memory.service';
+
+@Global()
+@Module({
+    imports: [UserModule],
+    providers: [
+        {
+            provide: UserRepository,
+            useClass: UserRepositorySQLInMemory,
+        },
+    ],
+    exports: [UserRepository],
+})
+export class TestDependenciesModule { }
