3a388302db9d329e8de3eee308174ee135efbd50
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-15 16:48:48 -0300

    feat: add services and module

--- src/modules/test/test-dependencies.module.ts
+++ src/modules/test/test-dependencies.module.ts
@@ -2,16 +2,29 @@ import { Global, Module } from '@nestjs/common';
 import { UserRepository } from '../../repositories/abstracts/user.repository';
 import { UserModule } from '../user/user.module';
 import { UserRepositorySQLInMemory } from './sql-in-memory/user-repository-sql-in-memory.service';
+import { TransferRepository } from '../../repositories/abstracts/transfer.repository';
+import { TransferRepositorySQLInMemory } from './sql-in-memory/transfer-repository-sql-in-memory.service';
+import { NotifyRepository } from '../../repositories/abstracts/notify.repository';
+import { NotifyRepositorySQLInMemory } from './sql-in-memory/notify-repository-sql-in-memory.service';
+import { TransferModule } from '../transfer/transfer.module';
 
 @Global()
 @Module({
-    imports: [UserModule],
+    imports: [UserModule, TransferModule],
     providers: [
         {
             provide: UserRepository,
             useClass: UserRepositorySQLInMemory,
         },
+        {
+            provide: TransferRepository,
+            useClass: TransferRepositorySQLInMemory,
+        },
+        {
+            provide: NotifyRepository,
+            useClass: NotifyRepositorySQLInMemory,
+        },
     ],
-    exports: [UserRepository],
+    exports: [UserRepository, TransferRepository, NotifyRepository],
 })
 export class TestDependenciesModule { }
