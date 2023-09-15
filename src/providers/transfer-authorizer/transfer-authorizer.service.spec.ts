3abfdbde1f47938f17a60707b1f70a2ec7f6296c
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-15 16:46:08 -0300

    test: create tests transfer authorizer provider

---
+++ src/providers/transfer-authorizer/transfer-authorizer.service.spec.ts
@@ -0,0 +1,27 @@
+import axios from "axios";
+import { TransferAuthorizerService } from "./transfer-authorizer.service";
+
+describe('TransferAuthorizerService', () => {
+  let transferAuthorizerService: TransferAuthorizerService;
+
+  beforeEach(() => {
+    transferAuthorizerService = new TransferAuthorizerService();
+
+    jest.spyOn(axios, 'get');
+  });
+
+  it('should be defined', () => {
+    expect(transferAuthorizerService).toBeDefined();
+  });
+
+  it('should return a message from the authorizer API', async () => {
+    const response = await transferAuthorizerService.execute();
+
+    expect(axios.get).toHaveBeenCalledWith(
+      'https://run.mocky.io/v3/8fafdd68-a090-496f-8c9a-3442cf30dae6',
+    );
+
+    expect(typeof response).toBe('string');
+  });
+
+});
