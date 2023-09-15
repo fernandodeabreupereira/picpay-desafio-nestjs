d534336db20ee047e6ab55894e66305091e10382
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-15 16:47:24 -0300

    feat: implements controller make transfer

---
+++ src/modules/transfer/use-cases/make-transfer/make-transfer.controller.ts
@@ -0,0 +1,43 @@
+import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiResponse,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { MakeTransferService } from './make-transfer.service';
+import { IController, returnHandle } from '../../../../interfaces/controller';
+import { IReturnUser } from '../../../../interfaces/return-user';
+import { LoggedInUser } from '../../../../validators/logged-in-user.decorator';
+import { MakeTransferDTO } from './dtos/make-transfer.dto';
+
+@Controller()
+export class MakeTransferController implements IController {
+  constructor (private readonly _makeTransferService: MakeTransferService) { }
+
+  @ApiTags('transfer')
+  @ApiBearerAuth()
+  @ApiUnauthorizedResponse()
+  @ApiBadRequestResponse()
+  @ApiResponse({ status: 402 })
+  @ApiInternalServerErrorResponse()
+  @ApiOkResponse()
+  @Post('transfer')
+  @HttpCode(200)
+  async handle (
+    @LoggedInUser() user: IReturnUser,
+    @Body() body: MakeTransferDTO,
+  ): Promise<returnHandle> {
+    const makeTransfer = await this._makeTransferService.execute({
+      account_id: user.id,
+      ...body,
+    });
+
+    return {
+      message: makeTransfer,
+    };
+  }
+}
