7bc5bc561c0de1afe51f87e8936d99c35bfdf8ae
Fernando Pereira <fernandodeabreupereira@gmail.com>
2023-09-22 11:09:47 -0300

    test: add prisma user repository test

---
+++ src/repositories/implementations/prisma/user/prisma-user-repository.spec.ts
@@ -0,0 +1,105 @@
+import { Test } from '@nestjs/testing';
+import { PrismaService } from '../prisma-client.service';
+import { IUser } from '../../../../models/user';
+import { PrismaUserRepository } from './prisma-user-repository';
+import { prismaServiceTest } from '../test/prisma-service-test';
+
+describe('PrismaUserRepository', () => {
+  let prismaService: PrismaService;
+  let prismaUserRepository: PrismaUserRepository;
+  let createdUser: IUser;
+  let expectedCreatedUser: object;
+
+  const userData: IUser = {
+    role: 'user',
+    full_name: 'test_user',
+    cpf: '08601711081',
+    email: 'test@gmail.com',
+    password: 'test',
+    balance: 0,
+  };
+
+  beforeAll(async () => {
+    const module = await Test.createTestingModule({
+      providers: [
+        {
+          provide: PrismaService,
+          useValue: prismaServiceTest,
+        },
+        PrismaUserRepository,
+      ],
+    }).compile();
+
+    prismaService = module.get<PrismaService>(PrismaService);
+
+    prismaUserRepository =
+      module.get<PrismaUserRepository>(PrismaUserRepository);
+
+    jest.spyOn(prismaUserRepository, 'create');
+    jest.spyOn(prismaUserRepository, 'findById');
+    jest.spyOn(prismaUserRepository, 'findByEmail');
+    jest.spyOn(prismaUserRepository, 'findByCPF');
+  });
+
+  afterAll(async () => {
+    // Como o notify é associado ao user, tem que limpar primeiro, se não dá erro
+    await prismaService.notify.deleteMany();
+
+    await prismaService.user.deleteMany();
+    await prismaService.$disconnect();
+  });
+
+  it('should be defined', () => {
+    expect(prismaService).toBeDefined();
+    expect(prismaUserRepository).toBeDefined();
+  });
+
+  it('should create a user', async () => {
+    createdUser = await prismaUserRepository.create(userData);
+
+    expectedCreatedUser = {
+      id: expect.any(String),
+      ...userData,
+      created_at: expect.any(Date),
+      updated_at: expect.any(Date),
+    };
+
+    const users = await prismaService.user.findMany();
+
+    expect(prismaUserRepository.create).toHaveBeenCalledWith(userData);
+    expect(users).toHaveLength(1);
+    expect(users[0]).toEqual(expectedCreatedUser);
+  });
+
+  it('should find a user by ID', async () => {
+    const userFound = await prismaUserRepository.findById(createdUser.id);
+
+    expect(prismaUserRepository.findById).toHaveBeenCalledWith(
+      createdUser.id,
+    );
+
+    expect(userFound).toEqual(expectedCreatedUser);
+  });
+
+  it('should find a user by email', async () => {
+    const userFound = await prismaUserRepository.findByEmail(
+      createdUser.email,
+    );
+
+    expect(prismaUserRepository.findByEmail).toHaveBeenCalledWith(
+      createdUser.email,
+    );
+
+    expect(userFound).toEqual(expectedCreatedUser);
+  });
+
+  it('should find a user by CPF', async () => {
+    const userFound = await prismaUserRepository.findByCPF(createdUser.cpf);
+
+    expect(prismaUserRepository.findByCPF).toHaveBeenCalledWith(
+      createdUser.cpf,
+    );
+
+    expect(userFound).toEqual(expectedCreatedUser);
+  });
+});
